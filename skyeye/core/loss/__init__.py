2b83e4ad81f2d8045375526181fbc78cb63c735a
Umaima Khan <umaimaaakhan0143@gmail.com>
2025-04-21 20:25:14 -0400

    Update __init__.py

--- skyeye/core/loss/__init__.py
+++ skyeye/core/loss/__init__.py
@@ -1,1 +1,21 @@
+"""
+Loss functions for training SkyEye object detection models
+"""
 
+from .functions import (
+    ComputeLoss, 
+    AerialDetectionLoss,
+    FocalLoss, 
+    BCEWithLogitsLoss, 
+    smooth_bce,
+    bbox_iou
+)
+
+__all__ = [
+    'ComputeLoss',
+    'AerialDetectionLoss',
+    'FocalLoss',
+    'BCEWithLogitsLoss',
+    'smooth_bce',
+    'bbox_iou'
+]
